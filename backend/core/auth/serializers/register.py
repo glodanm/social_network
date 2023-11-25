9b0a3192fb2faa9a344e29784ec4fa0e932aeb89
glodanm <glodan196915@gmail.com>
2023-11-25 12:54:53 +0200

    Fix user registration feature

--- backend/core/auth/serializers/register.py
+++ backend/core/auth/serializers/register.py
@@ -11,10 +11,10 @@ class RegisterSerializer(UserSerializer):
     password = serializers.CharField(max_length=128, min_length=8,
                                     write_only=True, required=True)
     
+
     class Meta:
         model = User
-        fields = ['id', 'bio', 'avatar', 'email',
-                'username', 'first_name', 'last_name', 'password']
-        
-        def create(self, validated_data):
-            return User.objects.create_user(**validated_data)
+        fields = ['id', 'bio', 'avatar', 'email', 'username', 'first_name', 'last_name', 'password']
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
